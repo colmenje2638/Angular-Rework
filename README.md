2c0ce0ea45bc31b961e1a0bcdf649037280e8af6
Jose Colmenares <colmenje2638@gmail.com>
2019-02-16 17:26:12 -0800

    firstone

---
+++ README.md
@@ -0,0 +1,21 @@
+
+README
+
+# TXT Angular 6 Challenge
+
+This is the starter template for the Angular 6 challenge.
+
+# Installing
+
+```
+npm install
+```
+
+## Authors
+
+* **Roberto Sanchez** - roberto@urbantxt.com 
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE.md](LICENSE.md) file for details
+
---
+++ src/app/app.component.ts
@@ -0,0 +1,33 @@
+import { Component } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { NgIf } from '@angular/common';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  public name = 'Roberto Sanchez';
+  public form: FormGroup;
+  // blogs start off empty
+  public blogs = [];
+
+  constructor(public _formBuilder: FormBuilder) {
+    this.form = this._formBuilder.group(
+      {
+        title: ['', Validators.required],
+        content: ['', Validators.required],
+      }
+    );
+  }
+
+  onSubmit() {
+    console.log(this.form.value);
+    // add logic here to print out new blogs
+    // hint update your array of blogs;
+    this.blogs.push(this.form.value)
+    this.form.reset();
+    }
+  }
+
